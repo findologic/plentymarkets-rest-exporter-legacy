e67be23a5fbef05822479c934564640d9dd7640f
Tomas Semaška <tomas.semaska@adeoweb.lt>
2017-04-20 14:23:01 +0300

    Log class was modified with logging levels logic

--- src/Log.php
+++ src/Log.php
@@ -29,10 +29,52 @@ class Log
         }
     }
 
+    /*
+     * Logger wrapper functions for logging messages to customer and internal logger by level (trace, debug, etc.)
+     *
+     * Wrapper functions allows easier modifications for logging in the future if some level message logging logic
+     * should be changed.
+     */
+
+    public function trace($message)
+    {
+        $this->customerLogger->trace($message);
+        $this->logger->trace($message);
+    }
+
+    public function debug($message)
+    {
+        $this->customerLogger->debug($message);
+        $this->logger->debug($message);
+    }
+
     public function info($message)
     {
-        //TODO: implement logic for logging messages to logger and customerLogger
+        $this->customerLogger->info($message);
+        $this->logger->info($message);
     }
+
+    public function warn($message)
+    {
+        $this->customerLogger->warn($message);
+        $this->logger->warn($message);
+    }
+
+    public function error($message)
+    {
+        $this->customerLogger->error($message);
+        $this->logger->error($message);
+    }
+
+    public function fatal($message)
+    {
+        $this->customerLogger->fatal($message);
+        $this->logger->fatal($message);
+    }
+
+    /*
+     * Specific messages handling functions
+     */
 
     /**
      * Log information about missing data
@@ -48,17 +90,19 @@ class Log
     }
 
     /**
+     * Handle exception logging
+     *
      * @param \Exception $e
      * @return $this
      */
     public function handleException($e)
     {
         if ($e instanceof CriticalException) {
-            $this->logger->fatal('Critical error: ' . $e->getMessage());
+            $this->fatal('Critical error: ' . $e->getMessage());
             die();
         }
 
-        $this->logger->warn('An error occurred while running the exporter: ' . $e->getMessage());
+        $this->warn('An error occurred while running the exporter: ' . $e->getMessage());
 
         return $this;
     }
