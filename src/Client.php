4ef6a8a4569f2413b525c2ec45fd8be2c4ccdd9f
Tomas Semaška <tomas.semaska@adeoweb.lt>
2018-01-26 15:30:13 +0200

    Client class exception fix

--- src/Client.php
+++ src/Client.php
@@ -623,12 +623,8 @@ class Client
 
                 $continue = false;
             } catch (\Exception $e) {
-                if ($e instanceof ThrottlingException) {
-                    throw $e;
-                }
-
                 // If call to api was not successful check if retry limit was reached to stop retry cycle
-                if ($count >= self::RETRY_COUNT) {
+                if ($e instanceof ThrottlingException || $count >= self::RETRY_COUNT) {
                     throw $e;
                 } else {
                     usleep(100000);
