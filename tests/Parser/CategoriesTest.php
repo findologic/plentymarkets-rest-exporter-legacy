77a85065a60576820a0cdc2e4fce619ea7811cfb
Ismail Hanli <i.hanli@findologic.com>
2018-08-21 15:05:17 +0200

    Add test case

--- tests/Parser/CategoriesTest.php
+++ tests/Parser/CategoriesTest.php
@@ -35,10 +35,8 @@ class CategoriesTest extends PHPUnit_Framework_TestCase
     public function parseProvider()
     {
         return array(
-            // No categories given, results should be empty
-            array(array(), array()),
-            // Categories data given but there is no results for configured language
-            array(
+            'No categories given' => array(array(), array()),
+            'No categories given for configured language' => array(
                 array(
                     'entries' => array(
                         array(
@@ -50,8 +48,7 @@ class CategoriesTest extends PHPUnit_Framework_TestCase
                 ),
                 array()
             ),
-            // Categories data given, results should contain array with category id => name
-            array(
+            'Categories data given' => array(
                 array(
                     'entries' => array(
                         array(
@@ -63,6 +60,18 @@ class CategoriesTest extends PHPUnit_Framework_TestCase
                     )
                 ),
                 array(1 => array('name' => 'Test', 'urlKey' => 'test', 'fullPath' => '/test/'))
+            ),
+            'No categories given for configured plenty ID' => array(
+                array(
+                    'entries' => array(
+                        array(
+                            'details' => array(
+                                array('categoryId' => 1, 'name' => 'Test 2', 'lang' => 'en', 'plentyId' => '2', 'nameUrl' => 'test-2', 'previewUrl' => 'http://example.com/test-2/'),
+                            )
+                        ),
+                    )
+                ),
+                array(1 => array('name' => 'Test 2', 'urlKey' => 'test-2', 'fullPath' => '/test-2/'))
             )
         );
     }
