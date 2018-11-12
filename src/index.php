769de69a143372fc0aea7d593fceed382328435c
Ismail Hanli <i.hanli@findologic.com>
2018-11-12 12:53:40 +0100

    Make index.php debug only

--- src/index.php
+++ src/index.php
@@ -5,29 +5,26 @@ ini_set('max_execution_time', 300);
 require_once '../vendor/autoload.php';
 require_once 'PlentyConfig.php';
 
-use Findologic\Plentymarkets\Config;
-
-$debug = false;
+use Findologic\Plentymarkets\Debugger;
 
 Logger::configure('Logger/import.xml');
 Logger::initialize();
+
 $log = Logger::getLogger('import.php');
 $customerLogger = Logger::getLogger('import.php');
+$debug = new Debugger($log);
 
-if (Config::DEBUG) {
-    $log->info('Initialising the plugin with DEBUG mode ON.', false);
-    $debug = new \Findologic\Plentymarkets\Debugger($log);
-}
+$log->info('Initialising the plugin with DEBUG mode ON.');
 
 $config = new PlentyConfig();
 
 $config->setUsername('username')
     ->setPassword('password')
     ->setDomain('www.store.com')
-    ->setMultishopId(10000)
-    ->setPriceId(1)
-    ->setRrpId(2) // price id for 'instead' field
     ->setMultishopId(0)
+    ->setAvailabilityId(5)
+    ->setPriceId(1)
+    ->setRrpId(7) // price id for 'instead' field
     ->setLanguage('EN'); // Language code for texts
 
 $registry = new \Findologic\Plentymarkets\Registry($log, $customerLogger);
@@ -36,6 +33,6 @@ $wrapper = new \Findologic\Plentymarkets\Wrapper\Csv();
 $exporter = new \Findologic\Plentymarkets\Exporter($client, $wrapper, $log, $customerLogger, $registry);
 $exporter->init();
 
-echo $exporter->getProducts(Config::NUMBER_OF_ITEMS_PER_PAGE);
+echo $exporter->getProducts();
 
 $debug->writeCallTimingLog();
