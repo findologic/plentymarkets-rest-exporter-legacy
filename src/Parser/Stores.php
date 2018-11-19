97a48bacd34109fdbbec816005ea58300163c4a5
Tomas Semaška <tomas.semaska@adeoweb.lt>
2018-11-19 14:24:10 +0200

    (PLENTY-22) Consider language for item url

--- src/Parser/Stores.php
+++ src/Parser/Stores.php
@@ -15,22 +15,53 @@ class Stores extends ParserAbstract implements ParserInterface
         }
 
         foreach ($data as $stores) {
-            $this->results[$stores['storeIdentifier']] = $stores['id'];
+            $this->results[$stores['storeIdentifier']] = [
+                'id' => $stores['id'],
+                'defaultLanguage' => $stores['configuration']['defaultLanguage'],
+                'languageList' => array_map('trim', explode(',', $stores['configuration']['languageList']))
+            ];
         }
 
         return $this->results;
     }
 
     /**
-     * @param string $identifier
-     * @return string
+     * @param int $identifier
+     * @return int|string
      */
     public function getStoreInternalIdByIdentifier($identifier)
     {
-        if (isset($this->results[$identifier])) {
-            return $this->results[$identifier];
+        if (isset($this->results[$identifier]['id'])) {
+            return $this->results[$identifier]['id'];
         }
 
         return $this->getDefaultEmptyValue();
+    }
+
+    /**
+     * @param int $identifier
+     * @return string
+     */
+    public function getStoreDefaultLanguage($identifier)
+    {
+        if (isset($this->results[$identifier]['defaultLanguage'])) {
+            return strtolower($this->results[$identifier]['defaultLanguage']);
+        }
+
+        return $this->getDefaultEmptyValue();
+    }
+
+    /**
+     * @param $identifier
+     * @param $language
+     * @return bool
+     */
+    public function isLanguageAvailableInStore($identifier, $language)
+    {
+        if (isset($this->results[$identifier]['languageList']) && in_array(strtolower($language), $this->results[$identifier]['languageList'])) {
+            return true;
+        }
+
+        return false;
     }
 }
